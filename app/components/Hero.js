ba32b7645065808016f368ac502ead0514572155
Tasnimul Haque <tasnimul.haque6@gmail.com>
2025-01-25 01:04:44 +0600

    Refactor Hero component layout for improved readability and visual hierarchy

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -20,11 +20,13 @@ const Hero = () => {
         <SwiperSlide>Slide 9</SwiperSlide>
       </Swiper> */}
       <div className="h-[90dvh] w-[98%] m-auto bg-[url('https://images.unsplash.com/photo-1534068590799-09895a701e3e?q=80&w=1412&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-cover bg-center rounded-xl">
-        <div className="flex flex-col items-center justify-center h-full">
-          <h1 className="text-6xl font-bold text-white">ScreenVerse</h1>
-          <p className="text-xl text-white">
-            The ultimate destination for movie enthusiasts
-          </p>
+        <div className="flex flex-col items-start justify-end h-full bg-gradient-to-t from-black to-transparent rounded-xl p-10">
+          <div className="space-y-4">
+            <h1 className="text-6xl font-bold text-white">ScreenVerse</h1>
+            <p className="text-xl text-white">
+              The ultimate destination for movie enthusiasts
+            </p>
+          </div>
         </div>
       </div>
     </div>
