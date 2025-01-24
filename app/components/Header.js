a62757f1b6c5b34f3db8027f929d50896ef498c5
Tasnimul Haque <tasnimul.haque6@gmail.com>
2025-01-24 22:49:07 +0600

    Add logo and update Header component for improved navigation

--- app/components/Header.js
+++ app/components/Header.js
@@ -26,6 +26,7 @@ import {
   PhoneIcon,
   PlayCircleIcon,
 } from "@heroicons/react/20/solid";
+import Link from "next/link";
 
 const products = [
   {
@@ -68,20 +69,16 @@ export default function Example() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <header className="bg-white">
+    <header className="bg-white border-b border-gray-200 fixed inset-x-0 top-0 z-20">
       <nav
         aria-label="Global"
-        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+        className="mx-auto flex max-w-7xl items-center justify-between p-3 lg:px-5"
       >
-        <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
-            <span className="sr-only">Your Company</span>
-            <img
-              alt=""
-              src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
-              className="h-8 w-auto"
-            />
-          </a>
+        <div className="flex lg:flex-1 ">
+          <Link href="/" className="">
+            <span className="sr-only">ScreenVerse</span>
+            <img alt="" src="/screenverse.png" className="w-[40%] h-auto" />
+          </Link>
         </div>
         <div className="flex lg:hidden">
           <button
