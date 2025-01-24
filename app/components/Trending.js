24304f5ef9e57fe2b24e792efba58107c41959f9
Tasnimul Haque <tasnimul.haque6@gmail.com>
2025-01-25 00:59:39 +0600

    Implement async data fetching in Trending component for movie portfolios

--- app/components/Trending.js
+++ app/components/Trending.js
@@ -14,7 +14,23 @@ const movies = [
   },
 ];
 
-const Trending = () => {
+async function fetchPortfolios() {
+  try {
+    const res = await fetch("https://yts.mx/api/v2/list_movies.json");
+    if (!res.ok) {
+      throw new Error(`HTTP error! Status: ${res.status}`);
+    }
+    const portfolios = await res.json();
+    return portfolios;
+  } catch (error) {
+    console.error("Error fetching portfolios:", error);
+    return [];
+  }
+}
+
+const Trending = async () => {
+  const moviesBulk = await fetchPortfolios();
+  console.log(moviesBulk);
   return (
     <div className="py-16">
       <div className="w-11/12 mx-auto p-4 space-y-2">
